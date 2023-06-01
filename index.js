35b163901672bbaedd4e47a1b0ebc89c4094decd
deepanshudua555 <deepanshudua555@gmail.com.com>
2023-06-01 21:22:31 +0530

    change dir

---
+++ index.js
@@ -0,0 +1,18 @@
+import { app } from "./server/app.js";
+import { config } from "dotenv";
+import { connectDatabase } from "./server/config/database.js";
+import cloudinary from "cloudinary";
+
+config({
+  path: "./config/config.env",
+});
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
+});
